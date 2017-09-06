fbf46dab3163da8e04ea822a8b12197fc8710dd9
Matt <[mailprogramisty@gmail.com]>
2017-09-06 16:06:52 +0200

    JSX

---
+++ components/Contacts.js
@@ -0,0 +1,18 @@
+var Contacts = React.createClass({
+  propTypes: {
+    items: React.PropTypes.array.isRequired,
+  },
+
+  render: function() {
+    var contacts = this.props.items.map(function(contact) {
+        return {Contact} {item: contact} {{key: contact.id}}
+        React.createElement(Contact, {item: contact, key: contact.id});
+    });
+
+    return (
+      <ul className={'contactList'}>
+        {contacts}
+      </ul>
+    )
+  }
+});
