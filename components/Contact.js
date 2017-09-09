ba4b1874f21d288465abbd17bb9bc983f4bf7581
Michał Gacek <gacek.michael@outlook.com>
2017-09-09 17:46:38 +0200

    fixes

--- components/Contact.js
+++ components/Contact.js
@@ -5,13 +5,13 @@ var Contact = React.createClass({
 
   render: function() {
     return (
-      <div className={'contactItem'}>
-        <img className={'contacImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}/>
-        <p className={'contactLabel'}>
+      <div className="contactItem">
+        <img className="contactImage" src='http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico' />
+        <p className="contactLabel">
           Imię: {this.props.item.firstName}
           Nazwisko: {this.props.item.lastName}
         </p>
-        <a className={'contactEmail'} href={'mailto:' + this.props.item.email}>
+        <a className="contactEmail" href={'mailto:' + this.props.item.email}>
           {this.props.item.email}
         </a>
       </div>
